108bb2387ee91df266bec7f1e13868575b371137
Peter Jaszkowiak <p.jaszkow@gmail.com>
2024-04-14 17:37:45 -0600

    Reserve guarded string literals (RFC 3593)

--- compiler/rustc_lint/src/lints.rs
+++ compiler/rustc_lint/src/lints.rs
@@ -2974,3 +2974,10 @@ pub struct UnsafeAttrOutsideUnsafeSuggestion {
 pub struct OutOfScopeMacroCalls {
     pub path: String,
 }
+
+#[derive(LintDiagnostic)]
+#[diag(lint_reserved_string)]
+pub struct ReservedString {
+    #[suggestion(code = " ", applicability = "machine-applicable")]
+    pub suggestion: Span,
+}
