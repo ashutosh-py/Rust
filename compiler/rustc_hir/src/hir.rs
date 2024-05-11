c0e2e89875a572a4ef4e77a3e4b226d6c30c1ab8
The 8472 <git@infinite-source.de>
2024-05-11 18:18:51 +0200

    Add range information to slice metadata for types that are known to not be ZST
    # Conflicts:
    #	compiler/rustc_hir/src/hir.rs
    #	compiler/rustc_ty_utils/src/layout.rs
    #	tests/ui/stats/hir-stats.stderr

--- compiler/rustc_hir/src/hir.rs
+++ compiler/rustc_hir/src/hir.rs
@@ -3855,14 +3855,26 @@ impl<'hir> Node<'hir> {
 mod size_asserts {
     use super::*;
     use rustc_data_structures::static_assert_size;
-    // tidy-alphabetical-start
     static_assert_size!(Block<'_>, 48);
     static_assert_size!(Body<'_>, 24);
+    #[cfg(bootstrap)]
     static_assert_size!(Expr<'_>, 64);
+    #[cfg(not(bootstrap))]
+    static_assert_size!(Expr<'_>, 56);
+    #[cfg(bootstrap)]
     static_assert_size!(ExprKind<'_>, 48);
+    #[cfg(not(bootstrap))]
+    static_assert_size!(ExprKind<'_>, 40);
     static_assert_size!(FnDecl<'_>, 40);
+    #[cfg(bootstrap)]
     static_assert_size!(ForeignItem<'_>, 72);
+    #[cfg(not(bootstrap))]
+    static_assert_size!(ForeignItem<'_>, 64);
+    #[cfg(bootstrap)]
     static_assert_size!(ForeignItemKind<'_>, 40);
+    #[cfg(not(bootstrap))]
+    static_assert_size!(ForeignItemKind<'_>, 32);
+    // tidy-alphabetical-start
     static_assert_size!(GenericArg<'_>, 24);
     static_assert_size!(GenericBound<'_>, 48);
     static_assert_size!(Generics<'_>, 56);
