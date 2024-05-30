3d5ee2d66e06e84ac92d7b809619dc0c770e0a3d
Michael Goulet <michael@errs.io>
2024-05-30 13:29:54 -0400

    Implement RFC 3624 supertrait_item_shadowing

--- compiler/rustc_span/src/symbol.rs
+++ compiler/rustc_span/src/symbol.rs
@@ -1814,6 +1814,7 @@ symbols! {
         sub_assign,
         sub_with_overflow,
         suggestion,
+        supertrait_item_shadowing,
         surface_async_drop_in_place,
         sym,
         sync,
