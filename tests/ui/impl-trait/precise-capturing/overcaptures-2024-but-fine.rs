2938df1d3246dc64b43fc0e7c44f9bdc6971693b
Michael Goulet <michael@errs.io>
2024-08-12 17:56:49 -0400

    Don't worry about uncaptured contravariant lifetimes if they outlive a captured lifetime

---
+++ tests/ui/impl-trait/precise-capturing/overcaptures-2024-but-fine.rs
@@ -0,0 +1,16 @@
+//@ check-pass
+
+#![feature(precise_capturing)]
+#![deny(impl_trait_overcaptures)]
+
+struct Ctxt<'tcx>(&'tcx ());
+
+// In `compute`, we don't care that we're "overcapturing" `'tcx`
+// in edition 2024, because it can be shortened at the call site
+// and we know it outlives `'_`.
+
+impl<'tcx> Ctxt<'tcx> {
+    fn compute(&self) -> impl Sized + '_ {}
+}
+
+fn main() {}
