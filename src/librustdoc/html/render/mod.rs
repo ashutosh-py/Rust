dc67e7c201b9a826b311e45479618385dbab7127
EtomicBomb <ethan@ethan.ws>
2024-08-20 23:34:19 +0000

    rfc#3662 changes under unstable flags
    * All new functionality is under unstable options
    * Adds `--merge=shared|none|finalize` flags
    * Adds `--parts-out-dir=<crate specific directory>` for `--merge=none`
    to write cross-crate info file for a single crate
    * Adds `--include-parts-dir=<previously specified directory>` for
    `--merge=finalize` to write cross-crate info files
    * update tests/run-make/rustdoc-default-output/rmake.rs golden

--- src/librustdoc/html/render/mod.rs
+++ src/librustdoc/html/render/mod.rs
@@ -60,6 +60,7 @@ use serde::{Serialize, Serializer};
 
 pub(crate) use self::context::*;
 pub(crate) use self::span_map::{collect_spans_and_sources, LinkFromSrc};
+pub(crate) use self::write_shared::*;
 use crate::clean::{self, ItemId, RenderedLink};
 use crate::error::Error;
 use crate::formats::cache::Cache;
