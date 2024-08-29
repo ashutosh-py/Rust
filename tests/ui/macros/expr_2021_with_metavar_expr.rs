f86146768ee0a83db1b49d89f21b6d4030904279
Eric Holk <eric@theincredibleholk.org>
2024-08-29 12:10:36 -0700

    Update tests/ui/macros/expr_2021_with_metavar_expr.rs

--- tests/ui/macros/expr_2021_with_metavar_expr.rs
+++ tests/ui/macros/expr_2021_with_metavar_expr.rs
@@ -2,7 +2,8 @@
 //@ aux-build: metavar_2021.rs
 //@ run-pass
 
-// This test captures the behavior of macro-generating-macros with fragment specifiers across edition b
+// This test captures the behavior of macro-generating-macros with fragment
+// specifiers across edition boundaries.
 
 #![feature(expr_fragment_specifier_2024)]
 #![feature(macro_metavar_expr)]
